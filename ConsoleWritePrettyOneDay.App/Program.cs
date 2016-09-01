c78c1aa6b57ac24562edfc6319c8b875c99cb9de
Adam Prescott <prescott.adam@gmail.com>
2016-09-01 16:24:06 -0400

    Initial commit
    Contains initial implementation for console spinner

---
+++ ConsoleWritePrettyOneDay.App/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using ConsoleWritePrettyOneDay;
+using Console = System.Console;
+
+namespace ConsoleWritePrettyOneDay.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Spinner.Wait(() => Thread.Sleep(5000), "waiting for sleep");
+
+            var task = Task.Run(() => Thread.Sleep(4000));
+            Spinner.Wait(task, "waiting for task");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleWritePrettyOneDay/Spinner.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace ConsoleWritePrettyOneDay
+{
+    public static class Spinner
+    {
+        private static char[] _chars = new[] { '|', '/', '-', '\\', '|', '/', '-', '\\' };
+
+        public static void Wait(Action action, string message = null)
+        {
+            Wait(Task.Run(action), message);
+        }
+
+        public static void Wait(Task task, string message = null)
+        {
+            WaitAsync(task, message).Wait();
+        }
+
+        public static async Task WaitAsync(Task task, string message = null)
+        {
+            int index = 0;
+            int max = _chars.Length;
+
+            if (!string.IsNullOrWhiteSpace(message) && !message.EndsWith(" "))
+            {
+                message += " ";
+            }
+
+            var timer = new Stopwatch();
+            timer.Start();
+            while (!task.IsCompleted)
+            {
+                await Task.Delay(35);
+                index = ++index % max;
+                Console.Write($"\r{message}{_chars[index]}");
+            }
+            timer.Stop();
+            Console.WriteLine($"\r{message}[{(timer.ElapsedMilliseconds / 1000m).ToString("0.0##")}s]");
+        }
+    }
+}
