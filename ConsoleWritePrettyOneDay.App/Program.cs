9e0cf17d0b8a7775e7e381e788d14bbd22c2bd0d
Adam Prescott <adam.prescott@skykick.com>
2017-10-03 09:45:22 -0400

    Added console output that describes various tests in demo app

--- ConsoleWritePrettyOneDay.App/Program.cs
+++ ConsoleWritePrettyOneDay.App/Program.cs
@@ -9,23 +9,55 @@ namespace ConsoleWritePrettyOneDay.App
     {
         static void Main(string[] args)
         {
-            Spinner.Wait(() => Thread.Sleep(2000), "waiting for sleep");
+            Console.WriteLine("A SPINNER FOLLOWED BY A WRITELINE");
+            Spinner.Wait(() => Thread.Sleep(1000));
+            Console.WriteLine();
+            Console.WriteLine("---");
 
-            var task = Task.Run(() => Thread.Sleep(2000));
+            Console.WriteLine("A SPINNER WRAPPED BY WRITE & WRITELINE");
+            Console.Write("Waiting:");
+            Spinner.Wait(() => Thread.Sleep(1000));
+            Console.WriteLine("DONE");
+            Console.WriteLine("---");
+
+            Console.WriteLine("A LAMBDA SPINNER W/ MESSAGE");
+            Spinner.Wait(() => Thread.Sleep(1000), "waiting for sleep");
+            Console.WriteLine("---");
+
+            Console.WriteLine("A TASK SPINNER W/ MESSAGE");
+            var task = Task.Run(() => Thread.Sleep(1000));
             Spinner.Wait(task, "waiting for task");
+            Console.WriteLine("---");
 
+            Console.WriteLine("A SPINNER W/ TASK THAT WRITES TO CONSOLE");
             Spinner.Wait(() =>
                 Task
                     .Run(new Action(() =>
                     {
-                        Thread.Sleep(1000);
+                        Thread.Sleep(250);
                         Console.WriteLine("1");
-                        Thread.Sleep(1000);
+                        Thread.Sleep(250);
                         Console.WriteLine("2");
-                        Thread.Sleep(1000);
+                        Thread.Sleep(250);
+                        Console.WriteLine("3");
+                    }))
+                    .Wait());
+            Console.WriteLine("---");
+
+            Console.WriteLine("A SPINNER W/ TASK THAT WRITES TO CONSOLE AND MESSAGE");
+            Spinner.Wait(() =>
+                Task
+                    .Run(new Action(() =>
+                    {
+                        Thread.Sleep(250);
+                        Console.WriteLine("1");
+                        Thread.Sleep(250);
+                        Console.WriteLine("2");
+                        Thread.Sleep(250);
                         Console.WriteLine("3");
                     }))
                     .Wait(), "waiting for work that writes to console");
+            Console.WriteLine("---");
 
             Console.WriteLine();
             Console.WriteLine("Press [enter] to exit.");
